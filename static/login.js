ddaee0608cc7e0b2c3e6ffc99df522c51b0cff4a
essioui <salahessioui@gmail.com>
2024-08-01 18:28:10 +0100

    envirement

--- static/login.js
+++ static/login.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
+
     const token = getCookie('token');
+
     const loginLink = document.getElementById('login-link');
 
     if (token) {
@@ -15,26 +17,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 document.getElementById('login-form').addEventListener('submit', async function(event) {
+
     event.preventDefault();
 
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
     const response = await fetch('/login', {
+
         method: 'POST',
         headers: {
+
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({ email, password })
     });
 
     if (response.ok) {
+
         const data = await response.json();
+
         document.cookie = `token=${data.access_token}; path=/`;
         window.location.href = '/';
+
     } else {
+
         const errorText = await response.json();
+
         document.getElementById('errorMessage').innerText = errorText.msg;
+        
         // after message error affiche link of login for inscript
         document.getElementById('login-link').style.display = 'block';
     }
